46e66880b35ede26ac6425a9b88cf12f002c2af1
zorenv <973249003@qq.com>
2016-11-15 22:15:00 +0800

    paper表中添加paperUserEmail字段

--- sql/papermanage.sql
+++ sql/papermanage.sql
@@ -28,7 +28,8 @@ CREATE TABLE `paper` (
   `paperWebFilePath` varchar(255) DEFAULT NULL,
   `paperExteriorURL` varchar(255) DEFAULT NULL,
   `paperRemark` varchar(255) DEFAULT NULL,
-  `uploadDate` varchar(255) DEFAULT NULL
+  `uploadDate` varchar(255) DEFAULT NULL,
+  `paperUserEmail` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,29 +39,8 @@ CREATE TABLE `paper` (
 
 LOCK TABLES `paper` WRITE;
 /*!40000 ALTER TABLE `paper` DISABLE KEYS */;
-INSERT INTO `paper` VALUES ('dfg','','null','null','null','null'),('fasfsdf','','null','null','null','null'),('test','','null','null','null','null'),('pppp','ooo','null','null','null','null'),('pppp','oooo','null','null','null','null'),('pppp','oooo','null','null','null','null'),('pppp','oooo','null','null','null','null'),('poiuytre','ertyui','null','null','null','null'),('你好我是论文','哈尔滨工业大学','null','null','null','null'),('你好我是论文','哈尔滨工业大学','null','null','null','null'),('发送到福','曹德福','null','null','阿斯顿发顺丰到付','null'),('发送到福','曹德福','null','null','阿斯顿发顺丰到付','null'),('舒服撒水电费','的说法是打发发生的','null','null','是打发斯蒂芬是打发斯蒂芬','null'),('舒服撒水电费','的说法是打发发生的','null','null','是打发斯蒂芬是打发斯蒂芬','null'),('dfg达哥','上打广告方式','null','null','是个大范甘迪发','null'),('dfg达哥','上打广告方式','null','null','是个大范甘迪发','2016-11-07 20:27:04'),('dfg达哥否','曹德福','null','null','爱上对方法萨芬的','2016-11-07 20:27:24'),('规范dfg达哥否','哈尔滨工业大学','d:upload1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:31:47'),('bu规范dfg达哥否','哈尔滨工业大学','d:upload1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:31:47'),('bu规范dfg达哥否','哈尔滨工业大学','d:\\upload\\1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:31:47'),('规范dfg达哥否','哈尔滨工业大学','d:/upload/1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:37:22'),('dfg达哥否否','曹德福','d:/upload/1140310115-Lab2-report.doc','null','阿斯顿发生打发打发','2016-11-07 20:37:46');
+INSERT INTO `paper` VALUES ('bu规范dfg达哥否','哈尔滨工业大学','d:\\upload\\1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:31:47',NULL),('规范dfg达哥否','哈尔滨工业大学','d:/upload/1140310115-Lab2-report.doc','null','梵蒂冈和对方回复的恢复规划规范','2016-11-07 20:37:22',NULL),('dfg达哥否否','曹德福','d:/upload/1140310115-Lab2-report.doc','null','阿斯顿发生打发打发','2016-11-07 20:37:46',NULL);
 /*!40000 ALTER TABLE `paper` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sample`
---
-
-DROP TABLE IF EXISTS `sample`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sample` (
-  `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sample`
---
-
-LOCK TABLES `sample` WRITE;
-/*!40000 ALTER TABLE `sample` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sample` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -96,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-15 21:15:18
+-- Dump completed on 2016-11-15 22:14:06
