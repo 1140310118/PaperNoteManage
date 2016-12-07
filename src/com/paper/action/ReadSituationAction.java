0268e8f46300e1e3624d5ac608191bb920e7214c
zorenv <973249003@qq.com>
2016-12-07 20:44:48 +0800

    通过paperID来修改论文的阅读情况

--- src/com/paper/action/ReadSituationAction.java
+++ src/com/paper/action/ReadSituationAction.java
@@ -11,10 +11,10 @@ public class ReadSituationAction {
 	String paperTable = "paper";
 	private DAO dao = new DAO();
 
-	public String changeReadSituation(String paperNickName, String paperReadSituation) {
+	public String changeReadSituation(String paperID, String paperReadSituation) {
 		// UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值
 		String sql = "UPDATE paper SET paperReadSituation = " + paperReadSituation + "WHERE paperNickName = "
-				+ paperNickName;
+				+ paperID;
 		ResultSet rS = dao.executeQuery(sql);
 		if(rS != null)
 			return "changeReadSituationsuccess";
