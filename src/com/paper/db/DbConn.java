9d49633eb3bbc0329d25def569e888c385f5f18f
zorenv <973249003@qq.com>
2016-12-07 23:23:56 +0800

    solve conflicts

--- src/com/paper/db/DbConn.java
+++ src/com/paper/db/DbConn.java
@@ -8,13 +8,8 @@ public class DbConn {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-<<<<<<< HEAD
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/papermanage", "root", "112122");
 
-=======
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/papermanage", "root","Aa123456");
->>>>>>> branch 'master' of https://github.com/zorenv/PaperNoteManage.git
 		} catch (ClassNotFoundException e) {
 			System.out.println("11111");
 			// TODO Auto-generated catch block e.printStackTrace();
