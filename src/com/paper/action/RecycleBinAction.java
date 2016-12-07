8dfdb1d1d012a2c3190fbe22d1a952202ba3068c
zorenv <973249003@qq.com>
2016-12-07 18:54:24 +0800

    增加RecycleBinAction方法，返回回收站中的paper

---
+++ src/com/paper/action/RecycleBinAction.java
@@ -0,0 +1,32 @@
+package com.paper.action;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.paper.db.DAO;
+import com.paper.model.User;
+
+public class RecycleBinAction {
+	// 表名
+	String userTable = "user";
+	String paperTable = "paper";
+	private static DAO dao = new DAO();
+	
+//	public static void main(String[] args) throws SQLException {
+//		String sql = "SELECT * FROM paper WHERE paperIsDeleted = 1";
+//		System.out.println(sql);
+//		ResultSet rS = dao.executeQuery(sql);
+//		System.out.println(rS.next());
+//	}
+	
+	public String showRecycleBin(){
+		String sql = "SELECT * FROM paper WHERE paperIsDeleted = 1";
+		System.out.println(sql);
+		ResultSet rS = dao.executeQuery(sql);
+		System.out.println(rS);
+		if (rS != null)
+			return "showRecycleBinsuccess";
+		return "showRecycleBinfail";
+	}
+
+}
